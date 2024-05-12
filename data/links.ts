a79d9fae63dfe02761d73a29fd8634bfec1d5331
Nikunj Champakbhai Rohit <nikunjchampakbhairohit@Nikunjs-Laptop.local>
2024-05-12 11:02:19 +0530

    add some more dummy data

--- data/links.ts
+++ data/links.ts
@@ -32,4 +32,29 @@ export const links = [
     title: "Apollo GraphQL",
     url: "https://apollographql.com",
   },
+  {
+    category: "Open Source",
+    description: "Fullstack React framework",
+    id: 5,
+    imageUrl: "https://github.com/vercel.png",
+    title: "Next.js",
+    url: "https://nextjs.org",
+  },
+  {
+    category: "Open Source",
+    description: "Next Generation ORM for TypeScript and JavaScript",
+    id: 6,
+    imageUrl: "https://github.com/prisma.png",
+
+    title: "Prisma",
+    url: "https://prisma.io",
+  },
+  {
+    category: "Open Source",
+    description: "Utility-fist css framework",
+    id: 7,
+    imageUrl: "https://github.com/tailwindlabs.png",
+    title: "TailwindCSS",
+    url: "https://tailwindcss.com",
+  }
 ];
